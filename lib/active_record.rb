70522d70f48f26393b2cd57a0e211c9bd2adb0da
Lance Ivy <lance@cainlevy.net>
2007-03-02 23:51:25 +0000

    slightly cleaned up rdoc output

--- lib/active_record.rb
+++ lib/active_record.rb
@@ -1,5 +1,5 @@
-module ActiveRecord
-  class Base
+module ActiveRecord # :nodoc:
+  class Base # :nodoc:
     unless method_defined? :to_label
     def to_label
       self.to_s
--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,66 +1,68 @@
-module ActionView::Helpers
-  module RecordSelectHelpers
-    # Print this from your layout to include everything necessary for RecordSelect to work.
-    # Well, not everything. You need Prototype too.
-    def record_select_includes
-      includes = ''
-      includes << stylesheet_link_tag('record_select/record_select')
-      includes << javascript_include_tag('record_select/record_select')
-      includes
-    end
-
-    # Adds a link on the page that toggles a RecordSelect widget from the given controller.
-    #
-    # *Options*
-    # +onselect+::  JavaScript code to handle selections client-side. This code has access to three variables: id, label, and container.
-    # +params+::    Extra URL parameters. If any parameter is a column name, the parameter will be used as a search term to filter the result set.
-    def link_to_record_select(name, controller, options = {})
-      options[:params] ||= {}
-      options[:params].merge!(:controller => controller, :action => :browse)
-      options[:onselect] = "function(id, label, container) {#{options[:onselect]}}" if options[:onselect]
-
-      link_to_function(name, %|RecordSelect.toggle(this, '#{url_for options[:params]}', #{h options[:onselect]})|)
-    end
-
-    # Adds a RecordSelect-based form field. The field submits the record's id using a hidden input.
-    #
-    # *Options*
-    # +controller+::  The controller configured to provide the result set. Optional if you have standard resource controllers (e.g. UsersController for the User model), in which case the controller will be inferred from the class of +current+ (the second argument)
-    # +id+::          The id to use for the hidden input. Defaults based on the input's name.
-    def record_select_field(name, current, options = {})
-      options[:controller] ||= current.class.to_s.pluralize.underscore
-      options[:id] ||= name.gsub(/[\[\]]/, '_')
-
-      label = (!current or current.new_record?) ? 'None Selected' : current.to_label
-
-      html = ''
-      html << %(<input type="hidden" name="#{h name}" value="#{current.id}" id="#{options[:id]}" />)
-      html << link_to_record_select(
-        "<span class='record-select-input'>#{h label}</span>",
-        options[:controller],
-        :onselect => "$('#{options[:id]}').value = id; Element.previous(container).childNodes[0].innerHTML = label; Element.remove(container);",
-        :params => options[:params]
-      )
-
-      return html
-    end
-
-    # A helper to render RecordSelect partials
-    def render_record_select(options = {})
-      if options[:partial]
-        render :partial => controller.send(:record_select_path_of, options[:partial]), :locals => options[:locals]
+module ActionView # :nodoc:
+  module Helpers # :nodoc:
+    module RecordSelectHelpers
+      # Print this from your layout to include everything necessary for RecordSelect to work.
+      # Well, not everything. You need Prototype too.
+      def record_select_includes
+        includes = ''
+        includes << stylesheet_link_tag('record_select/record_select')
+        includes << javascript_include_tag('record_select/record_select')
+        includes
       end
-    end
 
-    # Provides view access to the RecordSelect configuration
-    def record_select_config
-      controller.send :record_select_config
-    end
+      # Adds a link on the page that toggles a RecordSelect widget from the given controller.
+      #
+      # *Options*
+      # +onselect+::  JavaScript code to handle selections client-side. This code has access to three variables: id, label, and container.
+      # +params+::    Extra URL parameters. If any parameter is a column name, the parameter will be used as a search term to filter the result set.
+      def link_to_record_select(name, controller, options = {})
+        options[:params] ||= {}
+        options[:params].merge!(:controller => controller, :action => :browse)
+        options[:onselect] = "function(id, label, container) {#{options[:onselect]}}" if options[:onselect]
 
-    # The id of the RecordSelect widget for the given controller.
-    def record_select_id(controller = nil)
-      controller ||= params[:controller]
-      "record-select-#{controller}"
+        link_to_function(name, %|RecordSelect.toggle(this, '#{url_for options[:params]}', #{h options[:onselect]})|)
+      end
+
+      # Adds a RecordSelect-based form field. The field submits the record's id using a hidden input.
+      #
+      # *Options*
+      # +controller+::  The controller configured to provide the result set. Optional if you have standard resource controllers (e.g. UsersController for the User model), in which case the controller will be inferred from the class of +current+ (the second argument)
+      # +id+::          The id to use for the hidden input. Defaults based on the input's name.
+      def record_select_field(name, current, options = {})
+        options[:controller] ||= current.class.to_s.pluralize.underscore
+        options[:id] ||= name.gsub(/[\[\]]/, '_')
+
+        label = (!current or current.new_record?) ? 'None Selected' : current.to_label
+
+        html = ''
+        html << %(<input type="hidden" name="#{h name}" value="#{current.id}" id="#{options[:id]}" />)
+        html << link_to_record_select(
+          "<span class='record-select-input'>#{h label}</span>",
+          options[:controller],
+          :onselect => "$('#{options[:id]}').value = id; Element.previous(container).childNodes[0].innerHTML = label; Element.remove(container);",
+          :params => options[:params]
+        )
+
+        return html
+      end
+
+      # A helper to render RecordSelect partials
+      def render_record_select(options = {})
+        if options[:partial]
+          render :partial => controller.send(:record_select_path_of, options[:partial]), :locals => options[:locals]
+        end
+      end
+
+      # Provides view access to the RecordSelect configuration
+      def record_select_config
+        controller.send :record_select_config
+      end
+
+      # The id of the RecordSelect widget for the given controller.
+      def record_select_id(controller = nil)
+        controller ||= params[:controller]
+        "record-select-#{controller}"
+      end
     end
   end
 end
