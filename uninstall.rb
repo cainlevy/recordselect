c7f81fee0155aaede7bf998895850364049888a8
Lance Ivy <lance@cainlevy.net>
2007-03-02 22:35:51 +0000

    adding plugin uninstall.rb

--- uninstall.rb
+++ uninstall.rb
@@ -1,1 +1,4 @@
-# Uninstall hook code here
+['stylesheets', 'images', 'javascripts'].each do |asset_type|
+  public_dir = File.join(RAILS_ROOT, 'public', asset_type, 'record_select')
+  FileUtils.rm_r public_dir
+end
