817fa1052675b639f116d1ee5ddbb67cea84fbe2
Lance Ivy <lance@cainlevy.net>
2007-04-19 23:26:37 +0000

    the link_to helper now shares the new javascript with the form helper. hooray for object-oriented programming.

--- assets/javascripts/record_select.js
+++ assets/javascripts/record_select.js
@@ -1,108 +1,49 @@
 var RecordSelect = new Object();
 
-RecordSelect = {
-  notify: function (item) {
-    var e = Element.up(item, '.record-select-handler');
-    var onselect = e.onselect || e.getAttribute('onselect');
-    if (typeof onselect != 'function') onselect = eval(onselect);
-    if (onselect)
-    {
-      try {
-        onselect(item.parentNode.id.substr(2), item.innerHTML, e);
-      } catch(e) {
-        alert(e);
-      }
-      return false;
+RecordSelect.notify = function(item) {
+  var e = Element.up(item, '.record-select-handler');
+  var onselect = e.onselect || e.getAttribute('onselect');
+  if (typeof onselect != 'function') onselect = eval(onselect);
+  if (onselect)
+  {
+    try {
+      onselect(item.parentNode.id.substr(2), item.innerHTML, e);
+    } catch(e) {
+      alert(e);
     }
-    else return true;
-  },
-
-  open: function(obj, url, onselect) {
-    /* don't re-open */
-    if (Element.next(obj) && Element.hasClassName(Element.next(obj), 'record-select-container')) return;
-
-    var id = obj.getAttribute('container_id');
-    var insertion = new Insertion.Bottom(document.body, '<div class="record-select-container record-select-handler" id="' + id + '"></div>');
-    var e = $(id);
-
-    if (Element.hasClassName(obj, 'record-select-autocomplete')) Element.addClassName(e, 'record-select-autocomplete');
-
-    var offset = Position.cumulativeOffset(obj);
-    e.style.left = offset[0] + 'px';
-    e.style.top = (Element.getHeight(obj) + offset[1]) + 'px';
-
-    e.onselect = onselect;
-
-    new Ajax.Updater(e, url, {
-      evalScripts: true,
-      asynchronous: true,
-      insertion: Insertion.Bottom,
-      onSuccess: function() {
-        Element.observe(document.body, 'click', function(ev) {
-          if (!e || !e.parentNode) return;
-          if (Element.ancestors(Event.element(ev)).include(e)) return;
-          Element.remove(e);
-          delete e;
-        });
-      }
-    });
-  },
-
-  close: function(obj) {
-    Element.remove($(obj.getAttribute('container_id')));
-  },
-
-  toggle: function(obj, url, onselect) {
-    if (Element.next(obj) && Element.hasClassName(Element.next(obj), 'record-select-container')) RecordSelect.close(obj);
-    else RecordSelect.open(obj, url, onselect);
+    return false;
   }
+  else return true;
 }
 
-
-
-
-
-
-RecordSelect.Autocomplete = Class.create();
-Object.extend(RecordSelect.Autocomplete.prototype, {
+RecordSelect.Abstract = Class.create();
+Object.extend(RecordSelect.Abstract.prototype, {
   /**
-   * obj - the text input field
-   * url - the url to open the recordselect
-   * current - a hash with the current id and label, like {id: '', label: ''}
+   * obj - the id or element that will anchor the recordselect to the page
+   * url - the url to run the recordselect
+   * options - ??? (check concrete classes)
    */
-  initialize: function(obj, url, current) {
-    this.text_input = $(obj);
+  initialize: function(obj, url, options) {
+    this.obj = $(obj);
     this.url = url;
-
-    // create the hidden input
-    new Insertion.After(this.text_input, '<input type="hidden" name="" value="" />')
-    this.hidden_input = this.text_input.next();
-
-    // transfer the input name from the text input to the hidden input
-    this.hidden_input.name = this.text_input.name;
-    this.text_input.name = '';
-
-    // initialize the values
-    this.set(current.id, current.label);
+    this.options = options;
+    this.container;
 
     Event.observe(window, 'load', this.onload.bind(this));
   },
 
   /**
-   * IE doesn't want to do too much until the page has finished loading
+   * Finish the setup - IE doesn't like doing certain things before the page loads
+   * --override--
    */
-  onload: function() {
-    // initialize the container
-    this.container = this.create_container();
+  onload: function() {},
 
-    // attach the events to start this party
-    this.text_input.observe('focus', this.open.bind(this));
-
-    // the autosearch event
-    this.text_input.observe('keyup', function() {
-      if (!this.is_open()) return;
-      this.container.down('input[name=search]').value = this.text_input.value;
-    }.bind(this));
+  /**
+   * the onselect event handler - when someone clicks on a record
+   * --override--
+   */
+  onselect: function(id, value) {
+    alert(id + ': ' + value);
   },
 
   /**
@@ -126,9 +67,9 @@ Object.extend(RecordSelect.Autocomplete.prototype, {
    * positions and reveals the recordselect
    */
   show: function() {
-    var offset = Position.cumulativeOffset(this.text_input);
+    var offset = Position.cumulativeOffset(this.obj);
     this.container.style.left = offset[0] + 'px';
-    this.container.style.top = (Element.getHeight(this.text_input) + offset[1]) + 'px';
+    this.container.style.top = (Element.getHeight(this.obj) + offset[1]) + 'px';
 
     this.container.show();
   },
@@ -150,14 +91,6 @@ Object.extend(RecordSelect.Autocomplete.prototype, {
   },
 
   /**
-   * the onselect event handler - when someone clicks on a record
-   */
-  onselect: function(id, value) {
-    this.set(id, value);
-    this.close();
-  },
-
-  /**
    * when the user clicks outside the dropdown
    */
   onbodyclick: function(ev) {
@@ -165,7 +98,78 @@ Object.extend(RecordSelect.Autocomplete.prototype, {
     var elem = $(Event.element(ev));
     var ancestors = elem.ancestors();
     ancestors.push(elem);
-    if (ancestors.include(this.container) || ancestors.include(this.text_input)) return;
+    if (ancestors.include(this.container) || ancestors.include(this.obj)) return;
+    this.close();
+  },
+
+  /**
+   * creates and initializes (and returns) the recordselect container
+   */
+  create_container: function() {
+    new Insertion.Bottom(document.body, '<div class="record-select-container record-select-handler"></div>');
+    e = document.body.childNodes[document.body.childNodes.length - 1];
+    e.onselect = this.onselect.bind(this);
+    e.style.display = 'none';
+
+    return $(e);
+  }
+});
+
+/**
+ * Used by link_to_record_select
+ * The options hash should contain a onselect: key, with a javascript function as value
+ */
+RecordSelect.Dialog = Class.create();
+RecordSelect.Dialog.prototype = Object.extend(new RecordSelect.Abstract(), {
+  onload: function() {
+    this.container = this.create_container();
+    this.obj.observe('click', this.toggle.bind(this));
+  },
+
+  onselect: function(id, value) {
+    if (this.options.onselect(id, value) != false) this.close();
+  },
+
+  toggle: function() {
+    if (this.is_open()) this.close();
+    else this.open();
+  }
+});
+
+/**
+ * Used by record_select_field helper
+ * The options hash may contain id: and label: keys, designating the current value
+ */
+RecordSelect.Autocomplete = Class.create();
+RecordSelect.Autocomplete.prototype = Object.extend(new RecordSelect.Abstract(), {
+  onload: function() {
+    // create the hidden input
+    new Insertion.After(this.obj, '<input type="hidden" name="" value="" />')
+    this.hidden_input = this.obj.next();
+
+    // transfer the input name from the text input to the hidden input
+    this.hidden_input.name = this.obj.name;
+    this.obj.name = '';
+
+    // initialize the values
+    this.set(this.options.id, this.options.label);
+
+    // initialize the container
+    this.container = this.create_container();
+    this.container.addClassName('record-select-autocomplete');
+
+    // attach the events to start this party
+    this.obj.observe('focus', this.open.bind(this));
+
+    // the autosearch event
+    this.obj.observe('keyup', function() {
+      if (!this.is_open()) return;
+      this.container.down('input[name=search]').value = this.obj.value;
+    }.bind(this));
+  },
+
+  onselect: function(id, value) {
+    this.set(id, value);
     this.close();
   },
 
@@ -173,19 +177,7 @@ Object.extend(RecordSelect.Autocomplete.prototype, {
    * sets the id/label
    */
   set: function(id, label) {
-    this.text_input.value = label;
+    this.obj.value = label;
     this.hidden_input.value = id;
-  },
-
-  /**
-   * creates and initializes (and returns) the recordselect container
-   */
-  create_container: function() {
-    new Insertion.Bottom(document.body, '<div class="record-select-container record-select-handler record-select-autocomplete"></div>');
-    e = document.body.childNodes[document.body.childNodes.length - 1];
-    e.onselect = this.onselect.bind(this);
-    e.style.display = 'none';
-
-    return $(e);
   }
 });
--- lib/helpers.rb
+++ lib/helpers.rb
@@ -13,16 +13,19 @@ module ActionView # :nodoc:
       # Adds a link on the page that toggles a RecordSelect widget from the given controller.
       #
       # *Options*
-      # +onselect+::  JavaScript code to handle selections client-side. This code has access to three variables: id, label, and container.
+      # +onselect+::  JavaScript code to handle selections client-side. This code has access to two variables: id, label. If the code returns false, the dialog will *not* close automatically.
       # +params+::    Extra URL parameters. If any parameter is a column name, the parameter will be used as a search term to filter the result set.
       def link_to_record_select(name, controller, options = {})
         options[:params] ||= {}
         options[:params].merge!(:controller => controller, :action => :browse)
-        options[:onselect] = "function(id, label, container) {#{options[:onselect]}}" if options[:onselect]
+        options[:onselect] = "function(id, label) {#{options[:onselect]}}" if options[:onselect]
         options[:html] ||= {}
-        options[:html][:container_id] = record_select_container_id(controller)
+        options[:html][:id] ||= "rs_#{rand(9999)}"
 
-        link_to_function(name, %|RecordSelect.toggle(this, '#{url_for options[:params]}', #{h options[:onselect]})|, options[:html])
+        html = link_to_function(name, '', options[:html])
+        html << javascript_tag("new RecordSelect.Dialog(#{options[:html][:id].to_json}, #{url_for(options[:params]).to_json}, {onselect: #{options[:onselect]}})")
+
+        return html
       end
 
       # Adds a RecordSelect-based form field. The field submits the record's id using a hidden input.
@@ -43,6 +46,7 @@ module ActionView # :nodoc:
         end
 
         url = url_for({:action => :browse, :controller => options[:controller]}.merge(options[:params]))
+
         html = text_field_tag(name, nil, :autocomplete => 'off')
         html << javascript_tag("new RecordSelect.Autocomplete(#{options[:id].to_json}, #{url.to_json}, {id: #{id.to_json}, label: #{label.to_json}});")
 
@@ -69,10 +73,6 @@ module ActionView # :nodoc:
 
       def record_select_search_id(controller = nil)
         "#{record_select_id(controller)}-search"
-      end
-
-      def record_select_container_id(controller = nil)
-        "#{record_select_id(controller)}-container"
       end
     end
   end
