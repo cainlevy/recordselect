fee0279c229a2562cf1076e64131598a1a00db29
Lance Ivy <lance@cainlevy.net>
2007-08-20 19:36:40 +0000

    changing the API for the multi select so that it accepts a simple array of records, instead of an array of [id, label] pairs.

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -65,7 +65,7 @@ module ActionView # :nodoc:
       #
       # *Arguments*
       # +name+:: the input name that will be used to submit the selected records' ids. empty brackets will be appended to the name.
-      # +current+:: pass the current values in Rails' standard [[id, label], [id, label]] format.
+      # +current+:: pass a collection of existing associated records
       #
       # *Options*
       # +controller+::  The controller configured to provide the result set. Optional if you have standard resource controllers (e.g. UsersController for the User model), in which case the controller will be inferred from the class of +current+ (the second argument)
@@ -78,8 +78,7 @@ module ActionView # :nodoc:
 
         assert_controller_responds(options[:controller])
 
-        # convert each [id, label] pair into {:id => id, :label => label}
-        current = current.inject([]) { |memo, pair| memo.push({:id => pair[0], :label => pair[1]}) }
+        current = current.inject([]) { |memo, record| memo.push({:id => record.id, :label => record.to_label}) }
 
         url = url_for({:action => :browse, :controller => options[:controller]}.merge(options[:params]))
 
