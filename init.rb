4f3832a0a86cac96787b7c3d0bc24f7e1f8b99d2
Jorgen H. Fjeld <jorgen@veridit.no>
2009-06-02 17:03:14 +0800

    Moved record select helper to standard rails (>=2.3.2) engine placement.
    Signed-off-by: Lance Ivy <lance@cainlevy.net>

--- init.rb
+++ init.rb
@@ -2,7 +2,6 @@ require File.dirname(__FILE__) + '/lib/localization'
 require File.dirname(__FILE__) + '/lib/extensions/active_record'
 
 ActionController::Base.send(:include, RecordSelect)
-ActionView::Base.send(:include, RecordSelect::Helpers)
 ActionView::Helpers::FormBuilder.send(:include, RecordSelect::FormBuilder)
 
 ['stylesheets', 'images', 'javascripts'].each do |asset_type|
