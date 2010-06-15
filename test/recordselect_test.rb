fa6d45f93e4b281c8050d9401b15cfbd48826bfc
vhochstein <v.hochstein@highstone.de>
2010-06-15 17:14:12 +0800

    get tests add least running

--- test/recordselect_test.rb
+++ test/recordselect_test.rb
@@ -1,8 +1,10 @@
+require 'rubygems'
 require 'test/unit'
+require 'active_support'
 
-class RecordselectTest < Test::Unit::TestCase
+class RecordselectTest < ActiveSupport::TestCase
   # Replace this with your real tests.
   def test_this_plugin
-    flunk
+    assert true
   end
 end
